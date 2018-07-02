b8dffdec818d83ce146926bd5cd5164df514eab0
Arram Mandel <airum82@gmail.com>
2018-07-02 16:42:52 -0600

    Created and imported people button into App

--- swapi-box/src/App/App.js
+++ swapi-box/src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Intro } from '../Intro/Intro';
 import { FavoritesButton } from '../FavoritesButton/FavoritesButton';
+import { PeopleButton } from '../PeopleButton/PeopleButton';
 
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
           <h1>Swapi-box</h1>
           <FavoritesButton />
           <hr />
+          <PeopleButton />
         </main>
       </div>
     );
---
+++ swapi-box/src/PeopleButton/PeopleButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const PeopleButton = (props) => {
+  return (
+    <button>people</button>
+  )
+}
